b5aef71e539be885e3a8761c80ff81bb962c7c51
Anmol Kumar <ramuklomna100@gmail.com>
2024-12-26 23:17:10 +0530

    encoding

---
+++ ML/dataprocessing/split_train-test.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])  # calc mean
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+le = LabelEncoder()
+Y = le.fit_transform(Y)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
+
+print(X_train)
+print("------------------------------------------------------------------------------------------")
+print(X_test)
+print("------------------------------------------------------------------------------------------")
+print(Y_train)
+print("------------------------------------------------------------------------------------------")
+print(Y_test)
