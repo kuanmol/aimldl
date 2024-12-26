e8bab366f95774472d95168834f24115da5da8e3
Anmol Kumar <ramuklomna100@gmail.com>
2024-12-26 22:28:18 +0530

    encoding

---
+++ ML/dataprocessing/encoding-categorical.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])  #calc mean
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
