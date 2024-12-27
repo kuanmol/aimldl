01fc6c4d48a80ddb4019142f71fbb3c7f4e14023
Anmol Kumar <ramuklomna100@gmail.com>
2024-12-27 15:47:16 +0530

    prediction - plot

--- ML/Regression/simple-linear/prediction.py
+++ ML/Regression/simple-linear/prediction.py
@@ -3,13 +3,32 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-dataset=pd.read_csv('Salary_Data.csv')
-X=dataset.iloc[:,:-1].values
-Y=dataset.iloc[:,-1].values
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
 from sklearn.linear_model import LinearRegression
 
-regressor= LinearRegression()
-regressor.fit(X_train,Y_train)
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+y_pred = regressor.predict(X_test)
+
+#training set
+plt.figure(1)  # Create the first figure
+plt.scatter(X_train, Y_train, color="red")
+plt.plot(X_train, regressor.predict(X_train), color="blue")
+plt.title("Salary vs Experience  (training set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+
+
+#test result set
+plt.figure(2)  # Create the first figure
+plt.scatter(X_test, Y_test, color="red")
+plt.plot(X_train, regressor.predict(X_train), color="blue")
+plt.title("Salary vs Experience (Test set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
