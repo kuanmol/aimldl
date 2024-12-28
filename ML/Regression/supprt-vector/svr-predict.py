c7135db859095b448cad3c13deb89732441cf151
Anmol Kumar <ramuklomna100@gmail.com>
2024-12-28 17:17:24 +0530

    svr

---
+++ ML/Regression/supprt-vector/svr-predict.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+y = y.reshape(len(y), 1)
+
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+# train svr model
+from sklearn.svm import SVR
+regressor = SVR(kernel="rbf")
+regressor.fit(X, y)
+
+#predict
