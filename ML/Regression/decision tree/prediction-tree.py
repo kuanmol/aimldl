bc4076608e9ff8262250bf65dac6f6e25146cf00
Anmol Kumar <ramuklomna100@gmail.com>
2024-12-29 20:35:10 +0530

    decision tree

--- ML/Regression/decision tree/prediction-tree.py
+++ ML/Regression/decision tree/prediction-tree.py
@@ -1,10 +1,11 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
-from sklearn.tree import DecisionTreeRegressor
-
 regressor = DecisionTreeRegressor(random_state=0)
 regressor.fit(X, y)
+
+regressor.predict([[6.5]]) #prediction
---
+++ ML/Regression/decision tree/visualize.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.tree import DecisionTreeRegressor
+
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X, y)
+
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
