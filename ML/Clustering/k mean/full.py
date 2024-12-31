f052c91c94c9516cb58b3d9547800d97fba2c928
Anmol Kumar <ramuklomna100@gmail.com>
2024-12-31 23:17:10 +0530

    logistic

--- ML/Clustering/k mean/full.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-dataset = pd.read_csv('Mall_Customers.csv')
-X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, -1].values
-
-print(X)
