e76cf7494fc21bf4f33cf3ba47ca4fc3f1399306
Anmol Kumar <ramuklomna100@gmail.com>
2025-01-01 16:12:34 +0530

    k mean

---
+++ ML/Clustering/Hierarchical Clustering/Dedogram.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+
