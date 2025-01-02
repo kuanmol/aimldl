a7826c3150fde16422d71b35d2d298def9d82a29
Anmol Kumar <ramuklomna100@gmail.com>
2025-01-03 02:29:46 +0530

    Ann

--- dfgh.py
+++ dfgh.py
@@ -1,9 +1,30 @@
-# Building Ann
-ann = Sequential()
-ann.add(Dense(units=6, activation="relu"))  # first hidden layer or adding input layer
-ann.add(Dense(units=6, activation="relu"))  # adding second hidden layer
-ann.add(Dense(units=1, activation="sigmoid"))  # output layer
+import tensorflow as tf
+from keras import Sequential
+from keras.src.layers import Conv2D, Flatten, Dense
+from keras_preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.layers import MaxPool2D
 
-# traning Ann
-ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])  # compiiling
-ann.fit(X_train, y_train, batch_size=32, epochs=100)  # training ann on training set
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set', target_size=(64, 64),
+                                                 batch_size=32, class_mode='binary')
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+cnn = tf.keras.models.Sequential()
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+cnn.add(tf.keras.layers.Flatten())
+cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
+cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+cnn.fit(x = training_set, validation_data = test_set, epochs = 25)
