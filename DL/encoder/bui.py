b1fd90c13b82cac3ff0254b90f0eebe2089ff3a1
kuanmol <ramuklomna100@gmail.com>
2025-01-15 01:19:59 +0530

    exp

--- DL/encoder/bui.py
+++ DL/encoder/bui.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
-import torch
 import torch.nn as nn
-import torch.nn.parallel
 import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
@@ -58,3 +56,22 @@ class SAE(nn.Module):
 sae = SAE()
 criterion = nn.MSELoss()
 optimizer = optim.RMSprop(sae.parameters(), lr=0.01, weight_decay=0.5)
+
+np_epoch = 200
+for epoch in range(1, np_epoch + 1):
+    train_loss = 0
+    s = 0.
+    for id_user in range(nb_users):
+        input = Variable(training_set[id_user]).unsqueeze(0)
+        target = input.clone()
+        if torch.sum(target.data > 0) > 0:
+            output = sae(input)
+            target.require_grad = False
+            output[target == 0] = 0
+            loss = criterion(output, target)
+            mean_corrector = nb_movies / float(torch.sum(target.data > 0) + 1e-10)
+            loss.backward()
+            train_loss += np.sqrt(loss.data[0] * mean_corrector)
+            s += 1.
+            optimizer.step()
+    print("epoch " + str(epoch) + " loss " + str(train_loss / s))
