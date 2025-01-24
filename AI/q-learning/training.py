047025f44d556272fb20ed703d381bde5c70e61f
kuanmol <ramuklomna100@gmail.com>
2025-01-25 00:29:33 +0530

    deepq

--- AI/q-learning/training.py
+++ AI/q-learning/training.py
@@ -58,5 +58,79 @@ class ReplayMemory(object):
         actions = torch.from_numpy(np.vstack([e[1] for e in experience if e is not None])).long().to(self.device)
         rewards = torch.from_numpy(np.vstack([e[2] for e in experience if e is not None])).float().to(self.device)
         next_states = torch.from_numpy(np.vstack([e[3] for e in experience if e is not None])).float().to(self.device)
-        dones = torch.from_numpy(np.vstack([e[4] for e in experience if e is not None]).astype(np.uint8)).float().to(self.device)
+        dones = torch.from_numpy(np.vstack([e[4] for e in experience if e is not None]).astype(np.uint8)).float().to(
+            self.device)
         return states, next_states, actions, rewards, dones
+
+
+class Agent():
+    def __init__(self, state_size, action_size):
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.state_size = state_size
+        self.action_size = action_size
+        self.local_qnetwork = Network(state_size, action_size).to(self.device)
+        self.target_qnetwork = Network(state_size, action_size).to(self.device)
+        self.optimizer = optim.Adam(self.local_qnetwork.parameters(), lr=learning_rate)
+        self.memory = ReplayMemory(replay_buffer_size)
+        self.t_step = 0
+
+    def step(self, state, action, reward, next_state, done):
+        self.memory.push((state, action, reward, next_state, done))
+        self.t_step = (self.t_step + 1) % 4
+        if self.t_step == 0:
+            if len(self.memory.memory) > minibatch_size:
+                experiences = self.memory.sample(100)
+                self.learn(experiences, discount_factor)
+
+    def act(self, state, epilson=0.):
+        state = torch.from_numpy(self).float().unsqueeze(0).to(self.device)
+        self.local_qnetwork.eval()
+        with torch.no_grad():
+            action_values = self.local_qnetwork(state)
+        self.local_qnetwork.train()
+        if random.random() > epilson:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
+
+    def learn(self, experience, discount_factor):
+        states, next_state, actions, rewards, dones = experience
+        next_qtarget = self.target_qnetwork(next_state).detach().max(1)[0].unsqueeze(1)
+        q_targets = rewards + (discount_factor + next_qtarget * (1 - dones))
+        q_expected = self.local_qnetwork(states).gather(1, actions)
+        loss = F.mse_loss(q_expected, q_targets)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        self.soft_update(self.local_qnetwork, self.target_qnetwork, interpolation_parameter)
+
+    def soft_update(self, local_model, target_model, interpolation_parameter):
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(
+                interpolation_parameter * local_param.data + (1.0 - interpolation_parameter) * target_param.data)
+
+
+agent = Agent(state_size, number_actions)
+
+number_episode = 2000
+maximum_number_timesteps_per_episode = 1000
+epsilon_starting_value = 1.0
+epsilon_ending_value = 1.0
+epsilon_decay_value = 0.995
+epsilon = epsilon_starting_value
+scores_on_100_epsilons = deque(maxlen=100)
+
+for episode in range(1, number_episode + 1):
+    state, _ = env.reset()
+    score = 0
+    for t in range(maximum_number_timesteps_per_episode):
+        action = agent.act(state, epsilon)
+        next_state, reward, done, _, _ = env.step(action)
+        agent.step(state, action, reward, next_state, done)
+        state = next_state
+        score += reward
+        if done:
+            break
+
+    scores_on_100_epsilons.append(score)
+    epsilon = max(epsilon_ending_value, epsilon_decay_value * epsilon)
